14eb30598506e928fad5f185e52b7702fb041498
wb-lhy <lihy@huayunsoft.com>
2019-08-30 14:22:47 +0800

    加入微信sdk

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../../actions/home'
 
-@connect(state => state.home, { ...actions })
+@connect(state => ({home: state.home}), { ...actions })
 
 class HomeIndex extends Component{
     render(){
